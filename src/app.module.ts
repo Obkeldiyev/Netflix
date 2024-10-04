43fd4f740d64d9b3677911af2c213060dc83bf34
din-sus <tuhtamatovdinmuhammad2@gmail.com>
2024-10-04 11:36:10 +0500

    director model added, entity, dto, controllers, routers done, typeorm pg connnected

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DirectorModule } from './director/director.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: '0490',
+    database: 'netflix',
+    entities: [],
+    synchronize: true,
+    autoLoadEntities: true
+  }),
+  DirectorModule],
   controllers: [AppController],
   providers: [AppService],
 })
